621a110371df5dc64cb4adc408aafc2f5bd075d6
sweetyue9045 <47917697+sweetyue9045@users.noreply.github.com>
2023-02-16 21:32:30 +0800

    add article

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,6 +1,8 @@
 import "../style/Article.css";
 import { Link } from "react-router-dom";
 import BANNER from "../assets/images/article_cover.png";
+import Btn from "../assets/images/downBtn.png";
+
 // import typeData from "../assets/jsons/personality.json";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
@@ -13,7 +15,13 @@ function Article() {
             <div className="article-container">
                 <div className="Banner">
                     <div className="intro">
-                        
+                        <div className="item title">文章解析</div>
+                        <div className="item content">
+                            透過各路心理學大師<br />解析不一樣的自己
+                        </div>
+                        <div className="item btn">
+                            <img src={Btn} alt="" />
+                        </div>
                     </div>
                     <img src={BANNER} alt="cover.png" />
                 </div>
