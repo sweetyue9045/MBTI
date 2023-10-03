58add3919f8e3bdbc85d8c05447d443d9a130ffa
sweetyue9045 <47917697+sweetyue9045@users.noreply.github.com>
2023-10-03 14:40:20 +0800

    article rwd

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -2,6 +2,7 @@ import "../style/Article.css";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import BANNER from "../assets/images/article_cover.png";
+import BANNER2 from "../assets/images/article_cover2.png";
 import Btn from "../assets/images/downBtn.png";
 
 // import typeData from "../assets/jsons/personality.json";
@@ -12,21 +13,30 @@ const Article = () => {
     useEffect(() => {
         document.body.scrollTo(0, 0);
     }, []);
+    var cover = "cover1";
+    if (document.body.clientWidth > 430) {
+        cover = "cover1"
+    }
+    else {
+        cover = "cover2"
+    }
     return (
         <>
             <Nav posi="fixed" />
             <div className="article-container">
                 <div className="Banner">
+                    <img src={BANNER} alt="cover.png" style={{ display: cover == "cover1" ? "block" : "none" }} />
+                    <img src={BANNER2} alt="cover.png" style={{ display: cover == "cover2" ? "block" : "none" }} />
+
                     <div className="intro">
                         <div className="item title">文章解析</div>
                         <div className="item content">
                             透過各路心理學大師<br />解析不一樣的自己
                         </div>
                         <div className="item btn">
-                            <img src={Btn} alt="" />
+                            <img src={Btn} alt=""  style={{ display: cover == "cover1" ? "block" : "none" }}/>
                         </div>
                     </div>
-                    <img src={BANNER} alt="cover.png" />
                 </div>
                 <div className="article-content">
                     Coming soon...
