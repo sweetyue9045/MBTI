c60051d2b486851428811db7ae9a63c5e431cdb9
sweetyue9045 <47917697+sweetyue9045@users.noreply.github.com>
2024-09-22 01:46:43 +0800

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,8 +10,8 @@ import LOGO2 from "../assets/images/nav_logo2.png";
 
 export default function Nav() {
     const ClickHandler = (event) => {
-		event.preventDefault();
-	};
+        event.preventDefault();
+    };
     var logo = "logo1";
     if (document.body.clientWidth > 430) {
         logo = "logo1"
@@ -19,6 +19,15 @@ export default function Nav() {
     else {
         logo = "logo2"
     }
+
+    const phone = document.body.clientWidth > 430 ? 'noPhone' : 'yesPhone';
+    useEffect(() => {
+        if (phone == 'yesPhone') {
+            document.querySelectorAll('#nav a').forEach(a => {
+                a.onclick = click_close;
+            });
+        };
+    }, [phone])
 
     var click_open = function (e) {
         var target_open = document.getElementById("ham_close");
@@ -32,9 +41,9 @@ export default function Nav() {
         target_open.classList.add("block");
         target_close.classList.add("none");
 
-        hamnav.style.display="flex";
-        nav.style.height="100vh";
-        nav.style.backgroundColor="#fff";
+        hamnav.style.display = "flex";
+        nav.style.height = "100vh";
+        nav.style.backgroundColor = "#fff";
         nav.style.position = "fixed";
 
         disableScroll()
@@ -53,37 +62,37 @@ export default function Nav() {
         target_open.classList.add("block");
         target_close.classList.add("none");
 
-        hamnav.style.display="none";
-        nav.style.height="10vw";
-        nav.style.backgroundColor="transparent";
+        hamnav.style.display = "none";
+        nav.style.height = "10vw";
+        nav.style.backgroundColor = "transparent";
         nav.style.position = "absolute";
 
         enableScroll()
     }
     return (
         <nav id="nav">
-            <div id="nav-container" className="nav-container" style={{  display: logo === "logo1" ? "flex":"none"}}>
-                <NavLink to="/" onClick={() => click_close()}>
+            <div id="nav-container" className="nav-container" style={{ display: logo === "logo1" ? "flex" : "none" }}>
+                <NavLink to="/">
                     <div className="logo">
-                        <img src={LOGO} alt="logo.png" style={{ display: logo === "logo1" ? "block" : "none" }}/>
+                        <img src={LOGO} alt="logo.png" style={{ display: logo === "logo1" ? "block" : "none" }} />
                         <img src={LOGO2} alt="logo.png" style={{ display: logo === "logo2" ? "block" : "none" }} />
                     </div>
                 </NavLink>
                 <ul className="menu">
-                    <li><NavLink to="/world" className="menu_link" onClick={() => click_close()}>
+                    <li><NavLink to="/world" className="menu_link">
                         <div className="menu_btn">世界觀</div>
                     </NavLink></li>
-                    <li><NavLink to="/article" className="menu_link" onClick={() => click_close()}>
+                    <li><NavLink to="/article" className="menu_link">
                         <div className="menu_btn">文章解析</div>
                     </NavLink></li>
-                    <li><NavLink to="/membership" className="menu_link" style={{cursor: "default", color:"#777"}} onClick={ClickHandler}>
-                        <div className="menu_btn" style={{border:"#777 solid 3px"}}>註冊</div>
+                    <li><NavLink to="/membership" className="menu_link" style={{ cursor: "default", color: "#777" }} onClick={ClickHandler}>
+                        <div className="menu_btn" style={{ border: "#777 solid 3px" }}>註冊</div>
                     </NavLink></li>
                 </ul>
             </div>
-            <div className="hambtn" style={{ display: logo === "logo2" ? "block":"none"}}>
-            <img id="ham_open" className="block" src={HAM_OPEN} alt="ham_open.png" onClick={click_open.bind(this)} />
-            <img id="ham_close" className="none" src={HAM_CLOSE} alt="ham_close.png" onClick={click_close.bind(this)}/>
+            <div className="hambtn" style={{ display: logo === "logo2" ? "block" : "none" }}>
+                <img id="ham_open" className="block" src={HAM_OPEN} alt="ham_open.png" onClick={click_open} />
+                <img id="ham_close" className="none" src={HAM_CLOSE} alt="ham_close.png" onClick={click_close} />
             </div>
         </nav>
     );
